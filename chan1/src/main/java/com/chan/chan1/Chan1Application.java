a2092913765a17e03ebd45db22984844570d06df
cbmja <cbmja@naver.com>
2024-03-14 15:25:23 +0900

    d

---
+++ chan1/src/main/java/com/chan/chan1/Chan1Application.java
@@ -0,0 +1,13 @@
+package com.chan.chan1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chan1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chan1Application.class, args);
+	}
+
+}
